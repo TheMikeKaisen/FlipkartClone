56d800e04262f08956ab7d8946661a288c6e755b
TheMikeKaisen <98148929+TheMikeKaisen@users.noreply.github.com>
2023-08-10 12:13:38 +0530

    added a new ## Adding a new route to display the products on home page

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { userSignup, userLogin } from '../controller/user-controller.js';
-
+import { getProducts } from '../controller/product-controller.js';
 
 const Router = express.Router();
 
@@ -8,5 +8,7 @@ const Router = express.Router();
 
 Router.post('/signup',userSignup);
 Router.post('/login', userLogin);
+
+Router.get('/products', getProducts);
 
 export default Router;
