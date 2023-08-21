6dfea2b56e72b7d95e1f1d14e4e3cfe32c7ab9f1
TheMikeKaisen <98148929+TheMikeKaisen@users.noreply.github.com>
2023-08-21 21:38:28 +0530

    Removing items from cart

--- AboutCommit.md
+++ AboutCommit.md
@@ -1,2 +1,7 @@
-- styled cart page. Added buttons. Imported utilities. Designed Price Details Component (totalView)
-- did calculations.
+## Removing items from cart
+
+- components/cart/CartItems.jsx 
+- import useDispatch from react-redux
+- import removeFromCart from redux/actions/cartActions.js
+- Inside the remove button tag, add the onClick propoerty which would call a function 'removeItemFromCart' with item.id as parameter.
+- inside the 'removeItemFromCart' , use dispatch function to call **removeFromCart** in cartActions to remove the item with the id provided in the function
--- src/components/cart/CartItem.jsx
+++ src/components/cart/CartItem.jsx
@@ -1,5 +1,6 @@
 import { Box, Button, Typography, styled } from "@mui/material";
-
+import {removeFromCart} from "../../redux/actions/cartActions"
+import {useDispatch} from 'react-redux';
 import { addEllipsis } from "../../utils/common-utils";
 
 import GroupedButton from "./ButtonGroup";
@@ -30,6 +31,13 @@ const Remove = styled(Button)`
 
 const CartItem = ({item}) => {
     const fassured = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/fa_62673a.png';
+
+    const dispatch = useDispatch();    
+
+    const removeItemFromCart = (id) => {
+        dispatch(removeFromCart(id));
+    }
+
     return (
         
         <Component>
@@ -47,7 +55,7 @@ const CartItem = ({item}) => {
                     <Box component = "span" style={{color:'#878787'}}><strike>₹{item.price.mrp}</strike></Box>&nbsp;&nbsp;&nbsp;
                     <Box component = "span" style={{color: '#388e3c'}}>{item.price.discount}</Box>
                 </Typography>
-                <Remove>Remove</Remove>
+                <Remove onClick={()=>removeItemFromCart(item.id)}>Remove</Remove>
             </Box>
         </Component>
        
